2768766a8d2c5ae7f4f210812277337fb41adf8b
Ashutosh mishra <ashutoshmishra8796@gmail.com>
2025-09-16 10:05:09 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Homepage from './Homepage'
+import Homepage from './Homepage1'
 
 function App() {
   const [count, setCount] = useState(0)
